d20d933fe346def6ea582c8c51eb1483ba7354b8
YUHUI ZHOU <hughzhoutrt@gmail.com>
2021-12-28 22:49:01 -0500

    importing your own files

---
+++ section3_node.js_module_system/app2.js
@@ -0,0 +1,5 @@
+const notes = require('./app2_utils');
+
+const note = notes();
+
+console.log(note);
---
+++ section3_node.js_module_system/app2_utils.js
@@ -0,0 +1,13 @@
+console.log('app2_utils.js');
+
+const name = "hugh";
+
+const add = function(a, b) {
+    return a + b ;
+}
+
+const getNotes = function () {
+    return 'Your notes...';
+}
+
+module.exports = getNotes;
