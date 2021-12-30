370cd8c5ce95cf074630006cd19074876457b04b
YUHUI ZHOU <hughzhoutrt@gmail.com>
2021-12-29 23:55:34 -0500

    importing npm files

--- section3_node.js_module_system/app.js
+++ section3_node.js_module_system/app.js
@@ -1,5 +1,9 @@
-const fs = require('fs');
+const validator = require('validator');
 
 // fs.writeFileSync('notes.txt','My name is Hugh!');
 
-fs.appendFileSync('./notes.txt', 'add something behide my name!');
+// fs.appendFileSync('./notes.txt', 'add something behide my name!');
+
+console.log(validator.isEmail('hugh@1234.com'));
+
+console.log(validator.isURL('http://hiyuhui.com'));
