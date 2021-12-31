699cdac26a7729aa5f959f296b270560e7f124bc
YUHUI ZHOU <hughzhoutrt@gmail.com>
2021-12-30 23:18:14 -0500

    Printing in Color

--- section3_node.js_module_system/app.js
+++ section3_node.js_module_system/app.js
@@ -1,9 +1,13 @@
-const validator = require('validator');
+// const validator = require('validator');
 
 // fs.writeFileSync('notes.txt','My name is Hugh!');
 
 // fs.appendFileSync('./notes.txt', 'add something behide my name!');
 
-console.log(validator.isEmail('hugh@1234.com'));
+// console.log(validator.isEmail('hugh@1234.com'));
 
-console.log(validator.isURL('http://hiyuhui.com'));
+// console.log(validator.isURL('http://hiyuhui.com'));
+
+import chalk from 'chalk';
+
+console.log(chalk.blue.bgRed.bold.inverse('Success!'));
