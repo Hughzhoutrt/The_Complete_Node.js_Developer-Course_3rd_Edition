56f9a60fde626e21adc6c251eb6de04862cb8c2b
YUHUI ZHOU <hughzhoutrt@gmail.com>
2022-01-01 21:28:46 -0500

    argument parsing with yargs:part 1

--- section4_file_system_and_command_line_args/app.js
+++ section4_file_system_and_command_line_args/app.js
@@ -1,13 +1,58 @@
-import chalk from 'chalk';
+// import chalk from 'chalk';
 
-console.log(chalk.blue('Hello world!'));
 
-// console.log(process.argv[2]);
+const yargs = require('yargs');
 
-const command = process.argv[2];
+// console.log(chalk.blue('Hello world!'));
 
-if (command === 'add') {
-    console.log('Adding note!');
-} else if (command === 'remove') {
-    console.log('Removing note!');
-}
+// console.log(process.argv);
+//Customize yargs version
+yargs.version('1.1.0');
+
+//creat add command 
+yargs.command({
+    command:'add',
+    description:'Add a new note',
+    handler: function(){
+        console.log('Add a new new note!');
+    }
+})
+
+//create remove command 
+yargs.command({
+    command:'remove',
+    description:'Remove a new note',
+    handler: function(){
+        console.log('Remove a new new note!');
+    }
+})
+
+//CHALLENGES:
+//create list command 
+yargs.command({
+    command:'list',
+    description:'List a new note',
+    handler: function(){
+        console.log('List a new new note!');
+    }
+})
+
+//create read command
+yargs.command({
+    command:'read',
+    description:'Read a new note',
+    handler: function(){
+        console.log('Read a new new note!');
+    }
+})
+// add, remove, read, list
+
+console.log(yargs.argv);
+
+// const command = process.argv[2];
+
+// if (command === 'add') {
+//     console.log('Adding note!');
+// } else if (command === 'remove') {
+//     console.log('Removing note!');
+// }
